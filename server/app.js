6b223c7bc89b185fe2bd270c40a4d641f3a362fa
Arindam Keswani <arindamkeswani@gmail.com>
2022-12-25 23:06:41 +0530

    Setting-up GraphQL

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql'); //used to make express and graphql interact
+
+const app = express();
+
+// We can pass options in the object below
+app.use('/graphql', graphqlHTTP({
+
+}))
+// This route, as it is now, will give the following output:
+// {"errors":[{"message":"GraphQL middleware options must contain a schema."}]}
+
+// This is because GraphQL needs a schema (which may by represented by a graph, as our data is traversable)
+// Express-graphql needs to know how our graph looks, for which is needs a Schema  
+
+app.listen(4000, ()=>{
+    console.log(`Listening on Port: 4000`);
+})

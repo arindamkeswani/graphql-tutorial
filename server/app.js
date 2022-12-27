64b17a9ad24f37fa250fddbe329f6a69ee6e8021
Arindam Keswani <arindamkeswani@gmail.com>
2022-12-27 22:46:09 +0530

    Root Query

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql'); //used to make express and graphql interact
+const schema = require("./schema/schema");
 
 const app = express();
 
 // We can pass options in the object below
 app.use('/graphql', graphqlHTTP({
-
+    schema: schema
 }))
 // This route, as it is now, will give the following output:
 // {"errors":[{"message":"GraphQL middleware options must contain a schema."}]}
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString} = graphql; //Required to define a new "type" or a schema
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql; //Required to define a new "type" or a schema
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -21,3 +21,30 @@ const BookType = new GraphQLObjectType({
 //"fields" property is a function to establish linkage between different Types, allowing us to refer to one type from another
 // fields is going to return the object that is defined within it 
 // For each field, define its type such as String (e.g. GraphQLString), Integer, etc.
+
+// A Schema is used to:
+// 1. Define a Type
+// 2. Define a relationship between Types
+// 3. Define Root Queries (The point at which we enter the graph)
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args){
+                //code to get data from DB or some other source
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})

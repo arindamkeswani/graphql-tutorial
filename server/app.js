296d50ea7b1d35d175b47381919b0f1ef2bdae80
Arindam Keswani <arindamkeswani@gmail.com>
2023-01-08 00:06:12 +0530

    Connecting to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql'); //used to make express and graphql interact
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+require('dotenv').config();
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to DB');
+})
 
 // We can pass options in the object below
 app.use('/graphql', graphqlHTTP({

7b49bf124c11a42a69ba6aaafbe901591404630c
Arindam Keswani <arindamkeswani@gmail.com>
2023-01-22 02:17:32 +0530

    Book list component skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
+import BookList from "./Components/BookList";
 
 
 function App() {
   return (
     <div className="App">
       <h1>GraphQL Beginner Tutorial (Reading list)</h1>
+      <BookList/>
     </div>
   );
 }

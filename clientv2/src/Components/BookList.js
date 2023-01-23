b891c98736243072ac6354b62d5e045790662871
Arindam Keswani <arindamkeswani@gmail.com>
2023-01-23 22:08:09 +0530

    Rendering book list to the component

--- clientv2/src/Components/BookList.js
+++ clientv2/src/Components/BookList.js
@@ -1,11 +1,12 @@
-import { gql } from '@apollo/client';
-import { graphql } from '@apollo/client/react/hoc';
+import { gql, useQuery } from '@apollo/client';
+// import { graphql } from '@apollo/client/react/hoc';
 
 
 const getBooksQuery = gql`
   {
     books{
       name
+      genre
       id
     }
   }
@@ -13,16 +14,28 @@ const getBooksQuery = gql`
 
 function BookList(props) {
 
-  console.log(props);
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  function displayBooks() {
+    let list = data.books.map(book => {
+      return <li key={book.id}>{book.name}</li>
+    })
+
+    return list;
+  }
 
   return (
     <div>
       <ul id="book-list">
-        <li>Book name</li>
+        {displayBooks()}
       </ul>
     </div>
   );
 }
 
 //graphql binds the query to the component, then we export it
-export default graphql(getBooksQuery)(BookList);
+// export default graphql(getBooksQuery)(BookList);
+export default BookList;

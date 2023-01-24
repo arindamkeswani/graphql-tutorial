1b6dfcb6142225c6ca8a5fb05370cf7b8c17f250
Arindam Keswani <arindamkeswani@gmail.com>
2023-01-24 22:55:37 +0530

    Updating Component State

--- clientv2/src/Components/AddBook.js
+++ clientv2/src/Components/AddBook.js
@@ -1,9 +1,16 @@
 import { useQuery } from '@apollo/client';
 import {getAuthorsQuery} from '../Queries/queries';
+import {useState} from 'react';
 
 
 
 function AddBook(props) {
+
+    const [details, setDetails] = useState({
+        name: "",
+        genre: "",
+        author: ""  
+    })
 
     const { loading, error, data } = useQuery(getAuthorsQuery);
 
@@ -17,22 +24,41 @@ function AddBook(props) {
         })
     }
 
+    function updateDetails(key, value){
+        setDetails({
+            ...details,
+            [`${key}`] : value
+        })
+    }
+
+    function submitForm(e){
+        e.preventDefault(); //prevent page refresh
+        console.log(details);
+    }
+
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={submitForm.bind(this)}>
 
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" />
+                <input type="text" name="name" value={details.name}
+                onChange={ (e) => {updateDetails(e.target.name, e.target.value)} }
+                />
             </div>
 
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" />
+                <input type="text" name="genre" value={details.genre}
+                onChange={ (e) => {updateDetails(e.target.name, e.target.value)} }
+                />
             </div>
 
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select name="author" value={details.author}
+                onChange={ (e) => {updateDetails(e.target.name, e.target.value)} }
+                
+                >
                     <option>Select Author</option>
                     {displayAuthors()}
                 </select>
